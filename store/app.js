397c1c97dc52196e2884b43ab85fff2aa23eebc8
Stefano Carnà <stefano.carna.dev@gmail.com>
2021-09-24 15:26:49 +0200

    Add Init scritps (Vuex)

--- store/app.js
+++ store/app.js
@@ -1,11 +1,31 @@
 export const state = function () {
   return {
     loading: false,
+    appInit: false,
   }
 }
 
 export const mutations = {
+  SET_APP_INIT(state, value = true) {
+    state.appInit = value
+  },
+
   SET_LOADING(state, waiting) {
     state.loading = waiting
+  },
+}
+
+export const actions = {
+  async initialize({ rootState, state, commit, dispatch }) {
+    // eslint-disable-next-line no-caller
+    console.log('initialize')
+
+    if (state.appInit) throw new Error('initialize called twice')
+
+    /* Load User Information */
+    if (rootState.auth.accessToken)
+      await dispatch('auth/fetchFullUser', null, { root: true })
+
+    commit('SET_APP_INIT')
   },
 }
